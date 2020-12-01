8c372a4fd73337348115ea745ba237df2f804e09
Emily <emilylin08@gmail.com>
2020-12-01 01:53:09 -0800

    merged product details

--- product_info.php
+++ product_info.php
@@ -18,31 +18,21 @@ if(mysqli_num_rows($result)===1){
 }
 ?>
 
-
-<<<<<<< Updated upstream
 <div align ='left'> <img src="<?php echo $product['ProductImage']?>" width="350" height="200"/></div>
 
 
 <div class="product-headings"><?=$product['ProductName']?>
         <br/> &dollar;<?=$product['Cost']?> </div>
 
-<div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
-
-<br/> <div class="product-description"><?=$product['ProductDescription']?></div>
-
-<button class="back-button-productpage" onclick="history.go(-1);" >Previous Page </button>
-=======
-<h1 class="name"><?=$product['ProductName']?></h1>
-        &dollar;<?=$product['Cost']?>
-<div><img src="<?php echo $product['ProductImage']?>" width="350" height="200"/></div>
-
 <form action="test-zone.php?page=cart" method="post">
             <label>Quantity</label>
             <input type="number" name="quantity" value="1" min="1" max="<?=$product['quantity']?>" placeholder="Quantity" required>
             <input type="hidden" name="product_id" value="<?=$product['ProductID']?>">
-            <input type="submit" value="Add To Cart">
+            <input type="submit" value="Add to Cart" class="btnAddAction" />
         </form>
->>>>>>> Stashed changes
+        <div class="product-description"><?=$product['ProductDescription']?></div>
+        
+<button class="back-button-productpage" onclick="history.go(-1);" >Previous Page </button>
 
 <!------ FOOTER-------->  
 <?php

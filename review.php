833a41856d9f749a92292a8d7ebeb7215657dd09
Dhr408 <75280712+Dhr408@users.noreply.github.com>
2020-12-03 15:46:39 -0800

    Add files via upload

---
+++ review.php
@@ -0,0 +1,147 @@
+<?php   ?>
+<?php   ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <title>Ratings and Reveiws</title>
+    <link rel="stylesheet" href="reviewstyle.css">
+    <link rel="stylesheet" href=""    </head>
+    
+    <body>
+    <div class="containter">
+        <div class= "rating-review">
+        <div class = "tri table-flex">
+            <table>
+            <tbody>
+                <tr>
+                <td>
+                    <div class="rnb rv1">
+                    <h3>3/5.0</h3>
+                    </div>
+                    <div class="pdt-rate">
+                    <div class="pro-rating">
+                        <div class="clearfix rating marts">
+                            <div class="rating-stars">
+                            <div class="grey-stars"></div>
+                            <div class="filled-stars" style="width:60%"></div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="rnrn">
+                    <p class="rars">No Reviews</p>
+                    </div>
+                    </td>
+                <td>
+                    <div class="rpb">
+                    <div class="rnpb">
+                     <label>5<i class="fa fa-star"></i></label> 
+                        <div class="ropb">
+                            <div class="ripb" style="width: 20%"></div>
+                        </div>
+                        <div class="label">(1)</div>
+                        </div>
+                         <div class="rnpb">
+                     <label>4<i class="fa fa-star"></i></label> 
+                        <div class="ropb">
+                            <div class="ripb" style="width: 50%"></div>
+                        </div>
+                        <div class="label">(1)</div>
+                        </div>
+                         <div class="rnpb">
+                     <label>3<i class="fa fa-star"></i></label> 
+                        <div class="ropb">
+                            <div class="ripb" style="width: 80%"></div>
+                        </div>
+                        <div class="label">(15)</div>
+                        </div>
+                         <div class="rnpb">
+                     <label>2<i class="fa fa-star"></i></label> 
+                        <div class="ropb">
+                            <div class="ripb" style="width: 30%"></div>
+                        </div>
+                        <div class="label">(11)</div>
+                        </div>
+                         <div class="rnpb">
+                     <label>1<i class="fa fa-star"></i></label> 
+                        <div class="ropb">
+                            <div class="ripb" style="width: 40%"></div>
+                        </div>
+                        <div class="label">(13)</div>
+                        </div>
+                    </div>
+                    </td>
+                <td>
+                    <div class="rrb">
+                    <p>Please Review Our Product</p>
+                        <button class="rbtn opmd"> Add Review</button>
+                    </div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <div class="review.modal" style="display: none">
+            <div class="review-bg"></div>
+            <div class="rnp">
+                <div class= "rpc">
+                <span><i class="far fa-times"></i></span>
+                </div>
+                <div class="rps" align="center">
+                    <i class="fa fa-star" data-index="0" style="display:none"></i>
+                    <i class="fa fa-star" data-index="1"></i>
+                    <i class="fa fa-star" data-index="2"></i>
+                    <i class="fa fa-star" data-index="3"></i>
+                    <i class="fa fa-star" data-index="4"></i>
+                    <i class="fa fa-star" data-index="5"></i>
+                </div>
+                <input type="hidden" value="" class="starRateV">
+                <input type="hidden" value="" class="rateDate">
+                <div class="rptf" align="center">
+                <input type="text" class="raterName" placeholder="Enter Your Name...">
+                </div>
+                <div class="rptf" align="center">
+                <textarea class="rateMsg" placeholder="Describe Shopping Experience"></textarea>
+                </div>
+                <div class="rate-error" align="center"></div>
+                <div class="rpsb" align="center">
+                <button class="rpbtn">Post Review</button>
+                </div>
+                </div>
+            </div>
+            <div class="bri">
+            <div class="uscm">
+                <div class="uscm-secs">
+                <div class="us-ing">
+                    <p>DH</p>
+                    </div>
+                    <div class="uscms">
+                    <div class="us-rate">
+                        <div class="pdt-rate">
+                    <div class="pro-rating">
+                        <div class="clearfix rating marts">
+                            <div class="rating-stars">
+                            <div class= "grey-stars"></div>
+                            <div class="filled-stars" style="width:60%"></div>
+                            </div></div>
+                        </div>
+                    </div>
+                        </div>
+                        <div class="us-cmt">
+                        <p>Great Product!!</p>
+                        </div>
+                        <div class="us-nm">
+                        <p><i>By <span class= "cmnm">Diana </span> on <span class="cmdt">Dec 1 2020</span></i></p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <script src="techcity"></script>
+    </body>
+    <html></html>

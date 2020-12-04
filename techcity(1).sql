a2650d585051165c78ca36a2f1a979e1d3886c7f
Emily <emilylin08@gmail.com>
2020-12-04 02:42:29 -0800

    updated database with reviews + orders TABLE

--- techcity(1).sql
+++ techcity(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 04, 2020 at 11:39 AM
+-- Generation Time: Dec 04, 2020 at 11:41 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -82,6 +82,82 @@ INSERT INTO `inventory` (`ProductID`, `ProductName`, `ProductType`, `ProductDesc
 (5, 'Lenovo IdeaPad3 14\"', 'Laptop', 'Lenovo specifications', 5, '423.81', 'https://i.imgur.com/Zuj59r2.jpg', 6),
 (6, 'HP 15-dy1035nr', 'Laptop', 'HP specifications', 10, '597.50', 'https://i.imgur.com/b47prqw.jpg', 10);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderID` int(11) NOT NULL,
+  `CustomerID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `Cost` decimal(10,2) NOT NULL,
+  `OrderDate` date NOT NULL,
+  `OrderStatus` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`OrderID`, `CustomerID`, `ProductID`, `Cost`, `OrderDate`, `OrderStatus`) VALUES
+(1, 1, 4, '365.00', '2020-12-01', 'Delivered'),
+(2, 1, 2, '1319.00', '2020-11-30', 'Cancelled'),
+(3, 4, 4, '365.00', '2020-11-28', 'In progress'),
+(4, 3, 5, '424.00', '2020-11-15', 'Cancelled'),
+(6, 0, 5, '424.00', '2020-12-03', 'In Progress'),
+(7, 0, 5, '424.00', '2020-12-03', 'In Progress'),
+(8, 12, 4, '365.00', '2020-12-03', 'In Progress'),
+(9, 12, 6, '5975.00', '2020-12-03', 'In Progress'),
+(13, 12, 6, '5975.00', '2020-12-03', 'In Progress'),
+(14, 12, 1, '2149.00', '2020-12-03', 'In Progress'),
+(15, 0, 5, '423.81', '2020-12-03', 'In Progress'),
+(19, 0, 0, '0.00', '2020-12-03', 'In Progress'),
+(20, 0, 0, '0.00', '2020-12-04', 'In Progress'),
+(21, 0, 0, '0.00', '2020-12-04', 'In Progress'),
+(22, 0, 0, '1318.63', '2020-12-04', 'In Progress'),
+(23, 0, 0, '1318.63', '2020-12-04', 'In Progress'),
+(24, 0, 0, '1318.63', '2020-12-04', 'In Progress');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `ReviewID` int(11) NOT NULL,
+  `Customer_ID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `ReviewDate` date NOT NULL,
+  `ReviewTime` time NOT NULL,
+  `ReviewContent` varchar(100) DEFAULT NULL,
+  `ReviewRating` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`ReviewID`, `Customer_ID`, `ProductID`, `ReviewDate`, `ReviewTime`, `ReviewContent`, `ReviewRating`) VALUES
+(1, 1, 2, '2020-12-01', '10:28:22', 'Awesome Product!', '5'),
+(2, 3, 2, '2020-11-25', '14:37:36', 'Product kept on blue screening!', '1'),
+(3, 5, 2, '2020-12-02', '11:10:20', 'Cheap and affordable', '4'),
+(4, 4, 2, '2020-11-28', '12:00:00', 'Met my needs for class', '5'),
+(5, 1, 1, '2020-12-09', '11:40:00', 'I would give this a 0 if I could.', '1'),
+(6, 2, 1, '2020-11-30', '23:38:31', 'Great!', '5'),
+(7, 3, 1, '2020-12-25', '05:41:37', 'Alright for the price', '4'),
+(8, 4, 1, '2020-11-27', '13:23:37', 'You get what you expect', '2'),
+(9, 1, 3, '2020-11-27', '20:39:27', 'DO NOT BUY', '1'),
+(10, 3, 3, '2020-11-10', '13:21:40', 'Cheaply made', '1'),
+(11, 4, 3, '2020-12-14', '14:09:38', 'Works well', '4'),
+(12, 5, 3, '2020-11-22', '22:24:50', 'Works fine', '4'),
+(13, 6, 3, '2020-11-11', '13:13:00', 'so-so', '3'),
+(14, 1, 3, '2020-11-28', '10:00:41', 'Flimsy screen', '3'),
+(15, 2, 4, '2020-11-04', '14:27:18', 'Wonderful!', '5'),
+(16, 3, 4, '2020-11-13', '11:59:39', 'Apple rocks!', '5');
+
 --
 -- Indexes for dumped tables
 --
@@ -99,6 +175,20 @@ ALTER TABLE `inventory`
   ADD PRIMARY KEY (`ProductID`);
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderID`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`ReviewID`),
+  ADD KEY `Customer_ID` (`Customer_ID`),
+  ADD KEY `Customer_ID_2` (`Customer_ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -113,6 +203,18 @@ ALTER TABLE `customer`
 --
 ALTER TABLE `inventory`
   MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `ReviewID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

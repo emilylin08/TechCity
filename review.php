82c270bc95a4475b8ff0121119fc402f6cc8fe1d
Dhr408 <75280712+Dhr408@users.noreply.github.com>
2020-12-03 22:07:50 -0800

    Add files via upload

--- review.php
+++ review.php
@@ -1,14 +1,43 @@
-<?php   ?>
-<?php   ?>
+<?php
+    $mytime = getdate(date("U"));
+    $date = "$mytime[weekday], $mytime[month] $mytime[myday], $mytime[year]";
+
+    require "db_conn.php";
+        
+        $sql = $conn->query("SELECT customer_ID FROM reviews");
+        $numR = $sql->num_rows;
+
+        $sql = $conn->query("SELECT SUM(ReviewID) AS total FROM reviews");
+        $data = $sql->fetch_array();
+        $total = $data["total"];
+
+        $avg = "";
+
+        if($numR !=0){
+            if(is_nan(round(($total / $numR), 1))){
+                $avg = 0;
+            }
+            else{
+                $avg = round( ($total / $numR),1);
+            }
+        }
+            else{
+                $avg = 0;
+            }
+
+?>
+<?php ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width-device-width, initial-scale=1.0">
-    <title>Ratings and Reveiws</title>
+    <title>Product Reviews</title>
     <link rel="stylesheet" href="reviewstyle.css">
-    <link rel="stylesheet" href=""    </head>
+    <link rel="stylesheet" href="/your-path-to-fontawesome/css/fontawesome.css">
+    <script src = "http://code.jquery.com/jquery-3.2.1.min.js" integrity= "sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin= "anonymous"></script>
+    </head>
     
     <body>
     <div class="containter">
@@ -19,20 +48,20 @@
                 <tr>
                 <td>
                     <div class="rnb rv1">
-                    <h3>3/5.0</h3>
+                    <h3><?php echo $avg; ?></h3>
                     </div>
                     <div class="pdt-rate">
                     <div class="pro-rating">
                         <div class="clearfix rating marts">
                             <div class="rating-stars">
                             <div class="grey-stars"></div>
-                            <div class="filled-stars" style="width:60%"></div>
+                            <div class="filled-stars" style="width:<?php echo $avg * 20; ?>%"></div>
                             </div>
                         </div>
                         </div>
                     </div>
                     <div class="rnrn">
-                    <p class="rars">No Reviews</p>
+                    <p class="rars"><?php if($numR == 0){echo "NO";}else(echo $numR;) ?> Reviews</p>
                     </div>
                     </td>
                 <td>
@@ -83,7 +112,7 @@
                 </tr>
                 </tbody>
             </table>
-            <div class="review.modal" style="display: none">
+            <div class="review.modal" style="display:none">
             <div class="review-bg"></div>
             <div class="rnp">
                 <div class= "rpc">
@@ -98,7 +127,7 @@
                     <i class="fa fa-star" data-index="5"></i>
                 </div>
                 <input type="hidden" value="" class="starRateV">
-                <input type="hidden" value="" class="rateDate">
+                <input type="hidden" value="<?php echo $date;   ?>" class="rateDate">
                 <div class="rptf" align="center">
                 <input type="text" class="raterName" placeholder="Enter Your Name...">
                 </div>
@@ -113,9 +142,18 @@
             </div>
             <div class="bri">
             <div class="uscm">
+                <?php 
+                    $sqlp = "SELECT * FROM reviews";
+                    $result = $conn->query($sqlp);
+                    if(mysqli_num_rows($result) > 0){
+                        while($row = $result->fetch_assoc()){
+                            
+                        }
+                    }
+                ?>
                 <div class="uscm-secs">
                 <div class="us-ing">
-                    <p>DH</p>
+                    <p><?= substr($row['ReviewID'],0,1) ;?></p>
                     </div>
                     <div class="uscms">
                     <div class="us-rate">
@@ -124,24 +162,27 @@
                         <div class="clearfix rating marts">
                             <div class="rating-stars">
                             <div class= "grey-stars"></div>
-                            <div class="filled-stars" style="width:60%"></div>
+                            <div class="filled-stars" style="width:<?= $row['ReviewRating']*20; ?>%"></div>
                             </div></div>
                         </div>
                     </div>
                         </div>
                         <div class="us-cmt">
-                        <p>Great Product!!</p>
+                        <p><?= $row['ReviewContent']; ?></p>
                         </div>
                         <div class="us-nm">
-                        <p><i>By <span class= "cmnm">Diana </span> on <span class="cmdt">Dec 1 2020</span></i></p>
+                        <p><i>By <span class= "cmnm"><?= $row['ReviewID']; ?></span> on <span class="cmdt"><?= $row['ReviewDate']; ?></span></i></p>
                         </div>
                     </div>
                 </div>
+                <?php  }
+                    }
+                ?>
                 </div>
             </div>
             </div>
         </div>
         </div>
-        <script src="techcity"></script>
+        <script src="main.js"></script>
     </body>
     <html></html>
